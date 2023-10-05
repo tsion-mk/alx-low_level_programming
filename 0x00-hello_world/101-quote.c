51b33028ca50272cb954bb7bf06a70e688082421
tsion-mk <tsionmeka30@gmail.com>
2023-10-05 15:53:18 -0700

    More updates on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main 
+ * main
  * * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
