bef43b7eedfd54501f10a45a29c560f08b78d634
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-06 02:01:53 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main
+ * write(,,):prints a string to a standard output using write
  * * Return: 1 if success.
  */
 int main(void)
 {
-	/* prints a string to a standard output using write*/
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
