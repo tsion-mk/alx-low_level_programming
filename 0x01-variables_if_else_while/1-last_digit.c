275275fc602bb1434067d1bf75f6ffa5236c2670
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-10 00:44:53 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is grater than 5 ", n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0 ", n, last_digit);
+		printf("Last digit of %d is %d and is 0", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
 	printf("\n");
