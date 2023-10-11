1c94f2d8f03b12272b7bfb8cf6cf46739682ec9d
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-12 01:14:43 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,23 +11,30 @@ void print_times_table(int n)
 {
 	int i, j, mul;
 
-	for (i = 0; i <= n; i++)
+	if (n < 15 && n != 0)
 	{
-		_putchar('0');
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			mul = i * j;
-
-			if (mul > 9)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar('0' + (mul / 10));
-			}
-			else
+				_putchar(',');
 				_putchar(' ');
-			_putchar('0' + (mul % 10));
+				_putchar(' ');
+				mul = i * j;
+
+				if (mul > 9)
+				{
+					_putchar('0' + (mul / 10));
+				}
+				else if (mul > 99)
+					_putchar('0' + (mul /100);
+					_putchar('0' + (mul /10));
+				else
+					_putchar(' ');
+				_putchar('0' + (mul % 10));
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
