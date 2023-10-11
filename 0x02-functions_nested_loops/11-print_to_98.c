a3c05e10858a09443e5df43d3fdb1cde4d6ccb6c
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-11 23:53:49 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			if (n == 98)
-				printf("%d", n);
+				printf("%d ", n);
 			else
 				printf("%d, ", n);
 			n++;
@@ -27,7 +27,7 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			if (n == 98)
-				printf("%d", n);
+				printf("%d ", n);
 			else
 				printf("%d, ", n);
 			n--;
