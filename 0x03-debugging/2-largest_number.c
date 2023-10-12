20c1d837ef8e52c8f3b25ea5e2f37cd30d90b197
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-12 22:13:20 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,18 +17,15 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		printf("%d is the largest number\n", largest);
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		printf("%d is the largest number\n", largest);
 	}
 	else if (c > a && c > b)
 	{
 		largest = c;
-		printf("%d is the largest number\n", largest);
 	}
-
+	printf("%d is the largest number\n", largest);
 	return (largest);
 }
