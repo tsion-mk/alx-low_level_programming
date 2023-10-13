80f94f2b98b0f92e1c33052e58c5818ea06bb717
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-13 22:17:41 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,12 +4,12 @@
  * _isdigit - checks for a digit (0 through 9)
  * @c: a character to be checked
  *
- * Return: returns 1 if c is digit or 0 otherwise
+ * Return: returns 1 if c is a digit or 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
