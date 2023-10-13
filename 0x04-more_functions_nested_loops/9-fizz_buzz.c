9f1413e22af70c5568c51ae6be2e5ad00c656ffc
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-14 00:09:44 +0300

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,10 +17,10 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
-		else if (num % 5 == 0)
-			printf("Buzz");
 		else if (num % 3 == 0)
 			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d", num);
 		printf(" ");
