d2dc18793a6a4e5b4a105ebc146ee7d71d9e500c
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-14 09:46:15 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,13 @@ int main(void)
 	while (num <= 100)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz ");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz ");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz ");
 		else
-			printf("%d ", num);
+			printf(" %d ", num);
 		/*printf(" ");*/
 
 		num++;
