27cca7b73f59ba218837c2166d824a0a5382b2f9
tsion-mk <145681886+tsion-mk@users.noreply.github.com>
2023-10-20 20:44:44 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,20 +7,21 @@
 
 void print_rev(char *s)
 {
-	int i, j,len;
+	int i, j, len;
 
 	i = 0;
-	j = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	len = i;
-	j = len - 1;
-	while (j >= 0)
+
+	for (j = len - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
-		j--;
 	}
+
 	_putchar('\n');
 }
